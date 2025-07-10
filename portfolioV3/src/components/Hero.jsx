ebe9a82f52ce32e8391b1f2cfc328c4331ea2a14
softwareNinja-888 <mlandvodlamini03@gmail.com>
2025-07-10 15:27:29 +0200

    Refactor hero

--- portfolioV3/src/components/Hero.jsx
+++ portfolioV3/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { Profile } from './helper/Profile';
 import { Introduction } from './helper/Introduction'
 import { Menu, X } from 'lucide-react';
-import { Code } from "lucide-react"; 
+import { CodeXml } from "lucide-react"; 
 
 export function Line({ height = "h-20", color = "bg-gray-300", width = "w-px", className = "" }) {
   return <div className={`mx-auto ${width} ${height} ${color} ${className}`}></div>;
@@ -13,7 +13,7 @@ export function FramedImagePlaceholder() {
   return (
     <div className="relative w-40 h-40 sm:w-60 sm:h-60 lg:w-72 lg:h-72 border-4 border-primary rounded-sm">
       <div className="absolute -bottom-4 -right-4 bg-bg px-1 py-1 rounded">
-        <Code className="text-teal-400 w-6 h-6" />
+        <CodeXml className="text-teal-400 w-8 h-8 rotate-10" />
       </div>
     </div>
   );
